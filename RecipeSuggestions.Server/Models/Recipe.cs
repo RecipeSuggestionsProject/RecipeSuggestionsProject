dd6a8115141d879683fda1eb3725f0d1352f3636
Marilena Kosma <ice20390114@uniwa.gr>
2023-12-03 15:21:36 +0200

    Add Recipe class

---
+++ RecipeSuggestions.Server/Models/Recipe.cs
@@ -0,0 +1,11 @@
+﻿namespace RecipeSuggestions.Server.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public int Portions { get; set; }
+        public TimeSpan TotalTime { get; set; }
+    }
+}

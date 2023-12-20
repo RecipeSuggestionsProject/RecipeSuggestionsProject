7a91ed0367337879f73c2c1f3b6c4a436183bf56
Theofania Livanidi <ice20390131@uniwa.gr>
2023-12-20 10:25:11 +0200

    Ingredients_Recipes model added

---
+++ RecipeSuggestions.Server/Models/Ingredients_Recipes.cs
@@ -0,0 +1,9 @@
+﻿namespace RecipeSuggestions.Server.Models
+{
+    public class Ingredients_Recipes
+    {
+        public int RecipeId { get; set; }
+        public int IngredientId { get; set; }
+        public int Quantity { get; set; }
+    }
+}

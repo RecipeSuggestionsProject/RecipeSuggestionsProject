cd35eecb957222988f9a09aa93199964233b0e4d
Evangelos Spyropoulos <ice20390282@uniwa.gr>
2024-04-18 22:45:07 +0300

    Refactor AppSettings and related classes to exist once in a separate file in the Integration_Tests namespace

--- RecipeSuggestions.Server/Integration Tests/IngredientsControllerIntergrationTests.cs
+++ RecipeSuggestions.Server/Integration Tests/IngredientsControllerIntergrationTests.cs
@@ -63,28 +63,5 @@ namespace RecipeSuggestions.Server.Integration_Tests
             Assert.NotNull(_createdIngredient);
             Assert.NotNull(_ingredientContext.Ingredient.Find(_createdIngredient!.Id));
         }
-        
-    public class AppSettings
-    {
-        public Logging? Logging { get; set; }
-        public string? AllowedHosts { get; set; }
-        public ConnectionStrings? ConnectionStrings { get; set; }
     }
-
-    public class Logging
-    {
-        public LogLevel? LogLevel { get; set; }
-    }
-
-    public class LogLevel
-    {
-        public string? Default { get; set; }
-        public string? MicrosoftAspNetCore { get; set; }
-    }
-
-    public class ConnectionStrings
-    {
-        public string? RecipeSuggestionsServerContext { get; set; }
-    }
-}
 }
